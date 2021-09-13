94a562559ec313c7ffd7eff94448912e41345da8
Aadya <77448543+aadya1710@users.noreply.github.com>
2021-09-13 19:26:12 +0530

    Create sets.py
    we are taking a union, difference, intersection and symmetric differnce of sets here

---
+++ sets.py
@@ -0,0 +1,9 @@
+# first create two sets
+
+set1 = {0,2,4,6,8}
+set2 = {1,2,3,4,5}
+
+print("Union of two set is : ", set1 | set2)
+print("Intersection of two set is : ", set1 & set2)
+print("Difference of two set is : ", set1 - set2)
+print("Symmetric difference of two set is : ", set1 ^ set2)
